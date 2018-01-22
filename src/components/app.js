b1128ace801f23e38b05a87e92d7b596e96b36a7
Yoong Jian <iuncleblade_yj@hotmail.com>
2018-01-22 21:20:20 +0800

    Added fade in when hovered over item

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import MemeItem from './memeitem';
 
 class App extends Component {
   constructor() {
@@ -16,9 +17,7 @@ class App extends Component {
         {
           this.props.memes.slice(0, this.state.memeLimit).map((meme,index)=> {
             return (
-              <h4 key={index}>
-                {meme.name}
-              </h4>
+              <MemeItem key={index} meme={meme}/>
             )
           })
         }
