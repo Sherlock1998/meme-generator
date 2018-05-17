2caf5e16b0425f443d8be15c2d09939ed65edb7f
Sherlock1998 <iuncleblade_yj@hotmail.com>
2018-05-17 18:37:35 +0800

    Commit missing files

--- src/components/app.js
+++ src/components/app.js
@@ -1,25 +1,27 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import '../styles/index.css';
-import {Form,FormGroup,FormControl,ControlLabel} from 'react-bootstrap';
+import { Form, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 import MemeItem from './memeitem';
 import MyMemes from './mymemes';
 
 class App extends Component {
   constructor() {
-    super()
+    super();
 
-    this.state ={
-      memeLimit:10,
+    this.state = {
+      memeLimit: 10,
       text0: '',
-      text1: '',
-    }
+      text1: ''
+    };
   }
 
   render() {
     return (
       <div>
-        <h2><u>Meme Generator</u></h2>
+        <h2>
+          <u>Meme Generator</u>
+        </h2>
         <MyMemes />
         <h4>Write some Text</h4>
         <Form inline className="form">
@@ -27,39 +29,40 @@ class App extends Component {
             <ControlLabel>Top</ControlLabel>
             {'  '}
             <FormControl
-            type="text"
-            onChange={event => this.setState({text0: event.target.value})}
-            >
-            </FormControl>
+              type="text"
+              onChange={event => this.setState({ text0: event.target.value })}
+            />
           </FormGroup>
           {'  '}
           <FormGroup>
             <ControlLabel>Bottom</ControlLabel>
             {'  '}
             <FormControl
-            type="text"
-            onChange={event => this.setState({text1: event.target.value})}
-            >
-            </FormControl>
+              type="text"
+              onChange={event => this.setState({ text1: event.target.value })}
+            />
           </FormGroup>
         </Form>
-        {
-          this.props.memes.slice(0, this.state.memeLimit).map((meme,index)=> {
-            return (
-              <MemeItem
+        {this.props.memes.slice(0, this.state.memeLimit).map((meme, index) => {
+          return (
+            <MemeItem
               key={index}
               meme={meme}
               text0={this.state.text0}
               text1={this.state.text1}
-              />
-            )
-          })
-        }
-        <button className="btn btn-primary button-load--meme" onClick={()=>{
-          this.setState({memeLimit:this.state.memeLimit +10})
-        }}>Load More</button>
+            />
+          );
+        })}
+        <button
+          className="btn btn-primary button-load--meme"
+          onClick={() => {
+            this.setState({ memeLimit: this.state.memeLimit + 10 });
+          }}
+        >
+          Load More
+        </button>
       </div>
-    )
+    );
   }
 }
 
@@ -67,4 +70,4 @@ function mapStateToProps(state) {
   return state;
 }
 
-export default connect(mapStateToProps,null) (App);
+export default connect(mapStateToProps, null)(App);
